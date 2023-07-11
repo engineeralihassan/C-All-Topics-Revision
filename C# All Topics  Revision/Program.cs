d04ed3ae7ea545b5f330ed0d99c767dfad445ee6
Ali Hassan <engineralihassan@gmail.com>
2023-07-11 10:28:01 +0500

    The c# revision continue

--- C# All Topics  Revision/Program.cs
+++ C# All Topics  Revision/Program.cs
@@ -3738,22 +3738,22 @@ namespace _16_Structures
 /////////////////////////////////////////////////////////////////////////////////////////////////
 /// Guide
 /// /////////////////////////////////////////////////////////////////////////////////////////////
-namespace _19_Guide
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Globally unique identifier");
-            Guid g = Guid.NewGuid();
-            //Console.WriteLine(g.ToString().
-            //    Replace("-", string.Empty));
-            // Console.WriteLine(g.ToString("N"));
-            Console.WriteLine(g.ToString("N").Substring(0, 15));
-            Console.WriteLine(g);
+//namespace _19_Guide
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Globally unique identifier");
+//            Guid g = Guid.NewGuid();
+//            //Console.WriteLine(g.ToString().
+//            //    Replace("-", string.Empty));
+//            // Console.WriteLine(g.ToString("N"));
+//            Console.WriteLine(g.ToString("N").Substring(0, 15));
+//            Console.WriteLine(g);
 
-        }
-    }
-}
+//        }
+//    }
+//}
 
 
